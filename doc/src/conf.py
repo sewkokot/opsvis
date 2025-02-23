7f9524c415bd2c2f3462f733ad758546a38ccd36
Seweryn Kokot <sewkokot@gmail.com>
2025-02-23 21:01:44 +0100

    fix readthedocs

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -14,8 +14,6 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../'))
-
-import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'opsvis'
